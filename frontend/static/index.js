ab1542e6b2010fc4d533b2cf83f28a2424ea41bc
Tim Bauer <timbauer@ymail.com>
2021-08-02 12:59:49 -0700

    routing stuff

--- frontend/static/index.js
+++ frontend/static/index.js
@@ -1,1 +1,29 @@
-console.log("JS is loaded");
+const router = async () => {
+    const routes = [
+        { path: "/", view: () => console.log("viewing dashboard") },
+        { path: "/posts", view: () => console.log("viewing posts") },
+        { path: "/settings", view: () => console.log("viewing settings")}
+    ];
+
+    const potentialMatches = routes.map( route => {
+        return {
+            route: route,
+            isMatch: location.pathname === route.path
+        }
+    });
+
+    let match = potentialMatches.find(potentialMatches => potentialMatches.isMatch);
+
+    if(!match){
+        match = {
+            route: routes[0],
+            isMatch: true
+        }
+    }
+
+    console.log(match.route.view());
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+    router();
+})
